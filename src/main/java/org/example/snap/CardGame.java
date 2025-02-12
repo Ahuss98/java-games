32faa185faf903484956c895568d67c527f2f4d9
Ahmed Hussain <a.hmedhussain@hotmail.com>
2025-02-12 19:37:40 +0000

    adds fields constructors and populateDeck method to populate when class is constructed

--- src/main/java/org/example/snap/CardGame.java
+++ src/main/java/org/example/snap/CardGame.java
@@ -1,4 +1,33 @@
 package org.example.snap;
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 public class CardGame {
+
+    public ArrayList<Card> deckOfCards;
+    public String name;
+    public String name2;
+
+    public CardGame(String name){
+        this.name = name;
+        this.deckOfCards = new ArrayList<>();
+        populateDeck();
+
+    }
+
+    public CardGame(String name, String name2){
+        this.name = name;
+        this.name2 = name2;
+        this.deckOfCards = new ArrayList<>();
+        populateDeck();
+    }
+
+    public void populateDeck(){
+        String[] validSuits = {"hearts","diamonds","spades","clubs"};
+        for(int i = 0; i <4 ; i++){
+            for(int j = 2; j <= 14 ; j++){
+                deckOfCards.add(new Card(validSuits[i], j));
+            }
+        }
+    }
 }
