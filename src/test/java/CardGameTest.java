dbb3b12630b5a5e05e7fd7aeee4f9ffbd5825480
Ahmed Hussain <a.hmedhussain@hotmail.com>
2025-02-12 19:46:13 +0000

    testing for methods built so far in CardGame

---
+++ src/test/java/CardGameTest.java
@@ -0,0 +1,60 @@
+import org.example.snap.CardGame;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CardGameTest {
+    private CardGame cardGame;
+
+    @BeforeEach
+    void setup() {
+        cardGame = new CardGame("ahmed");
+    }
+
+    @Test
+    @DisplayName("Test populateDeck() should create a deck with 52 cards")
+    void populateDeck_CreatesFullDeck_DeckSizeIs52() {
+        assertEquals(52, cardGame.deckOfCards.size());
+    }
+
+    @Test
+    @DisplayName("Test getDeck describes every card in the deck")
+    void getDeck_DisplaysCorrectInformation() {
+        cardGame.getDeck();
+    }
+    @Test
+    @DisplayName("returns a different card depending on how many time dealCard has been called")
+    void dealCard_returnsCorrectCard(){
+        assertEquals("hearts", cardGame.dealCard().getSuit());
+        assertEquals(3, cardGame.dealCard().getValue());
+        cardGame.dealCard();
+        cardGame.dealCard();
+        cardGame.dealCard();
+        assertEquals(7,cardGame.dealCard().getValue());
+    }
+
+    @Test
+    @DisplayName("returns null when deck is empty")
+    void dealCard_dealEveryCard_returnsNull(){
+        for(int i = 0; i <= 52 ; i++){
+            cardGame.dealCard();
+        }
+        assertNull(cardGame.dealCard());
+    }
+    @Test
+    @DisplayName("sort the deck by value of cards")
+    void sortDeckInNumberOrder_sortEveryCardByValue_returnDeckOfCards(){
+        cardGame.sortDeckInNumberOrder();
+        cardGame.getDeck();
+        assertEquals(2,cardGame.dealCard().getValue());
+        cardGame.dealCard();
+        cardGame.dealCard();
+        cardGame.dealCard();
+        assertEquals(3,cardGame.dealCard().getValue());
+        cardGame.dealCard();
+        cardGame.dealCard();
+        cardGame.dealCard();
+        assertEquals(4,cardGame.dealCard().getValue());
+    }
+}
