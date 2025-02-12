91ee89f34e442e187330c5dea4c6b7d30686e615
Ahmed Hussain <a.hmedhussain@hotmail.com>
2025-02-12 20:29:05 +0000

    adding fields and constructors

--- src/main/java/org/example/snap/Snap.java
+++ src/main/java/org/example/snap/Snap.java
@@ -1,7 +1,25 @@
 package org.example.snap;
+import java.util.Scanner;
 
 public class Snap extends CardGame{
+    private Card lastCard;
+    private boolean isGameOver;
+    Scanner scanner ;
+    boolean isFirstCard = true;
+
     public Snap(String name) {
         super(name);
+        isGameOver = false;
+        scanner = new Scanner(System.in);
+        populateDeck();
+        shuffleDeck();
+    }
+
+    public Snap(String name1 , String name2){
+        super(name1,name2);
+        isGameOver = false;
+        scanner = new Scanner(System.in);
+        populateDeck();
+        shuffleDeck();
     }
 }
