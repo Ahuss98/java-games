09e2f483acc41e312ecd211be1c76b0cfb97e227
Ahmed Hussain <a.hmedhussain@hotmail.com>
2025-02-12 20:30:41 +0000

    eneabling user to play game with enter key, each turn a new card is dealt and the game conintues until the player wins

--- src/main/java/org/example/snap/Snap.java
+++ src/main/java/org/example/snap/Snap.java
@@ -22,4 +22,67 @@ public class Snap extends CardGame{
         populateDeck();
         shuffleDeck();
     }
+    public void playSnap(String name){
+        System.out.println("🔥 Welcome to Snap! Press ENTER to draw a card. Type 'exit' to quit.");
+
+        while(!isGameOver){
+            System.out.println("press enter to draw a new card");
+            String input = scanner.nextLine();
+            if(input.equals("exit")){
+                System.out.println("Thanks for playing");
+                break;
+            }
+
+            Card newCard = dealCard();
+            System.out.println(newCard.toString());
+
+            noMoreCardsChecker(newCard);
+            winChecker(newCard, name);
+        }
+    }
+    public void winChecker(Card newCard,String name){
+        if(!isFirstCard){
+            if(newCard != null && lastCard.getSymbol().equals(newCard.getSymbol())){
+                long startTime = System.currentTimeMillis();
+                while ((System.currentTimeMillis() - startTime) < 2000) {
+                    System.out.println( name + " type 'snap' as fast as you can");
+                    String input = scanner.nextLine().trim();
+                    if (input.equalsIgnoreCase("snap") && (System.currentTimeMillis() - startTime) < 2000) {
+                        System.out.println("🎉 SNAP!!! " + name + " WINS!");
+                        playAgain();
+                        return;
+                    } else if ((System.currentTimeMillis() - startTime) < 2000){
+                        System.out.println("❌ Wrong input! Try again.");
+                    } else {
+                        System.out.println("⌛ Too slow! You lost.");
+                        playAgain();
+                    }
+                }
+            } else {
+                lastCard = newCard;
+            }
+        } else {
+            lastCard = newCard;
+            isFirstCard = false;
+        }
+    }
+
+    public void noMoreCardsChecker(Card newCard){
+        if (newCard == null){
+            System.out.println("no cards left");
+            playAgain();
+        }
+    }
+    public void playAgain(){
+        System.out.println("type 'play' to play again");
+        String input = scanner.nextLine();
+        if(input.equals("play")){
+            populateDeck();
+            shuffleDeck();
+            playSnap(name);
+        } else {
+            System.out.println("Thanks for playing");
+            isGameOver = true;
+        }
+    }
 }
