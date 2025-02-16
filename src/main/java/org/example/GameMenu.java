0267f76e6d06a55959cf436a39c585a87d58a3ad
Ahmed Hussain <a.hmedhussain@hotmail.com>
2025-02-16 20:08:41 +0000

    adds exception handling for game selection in Gamemenu

--- src/main/java/org/example/GameMenu.java
+++ src/main/java/org/example/GameMenu.java
@@ -41,34 +41,61 @@ public class GameMenu {
     public void startGameWithPlayers(int numbOfPlayers) {
         if (numbOfPlayers == 1) {
             Player player = new Player();
-            scanner.nextLine();
             System.out.println("Please enter your name");
             player.setName(scanner.nextLine());
 
-            System.out.println("what game would you like to play? \n1.Snap \n2.Hangman");
-            int selected = scanner.nextInt();
-            if (selected == 1) {
-                newGame = new Snap(player);
-                newGame.playSnap(player);
-            } else if (selected == 2) {
-                HangmanMenu newGame = new HangmanMenu();
+
+            while (true) {
+                try {
+                    System.out.println("what game would you like to play? \n1.Snap \n2.Hangman");
+                    int selected = scanner.nextInt();
+                    scanner.nextLine();
+                    if (selected == 1) {
+                        newGame = new Snap(player);
+                        newGame.playSnap(player);
+                        break;
+                    } else if (selected == 2) {
+                        HangmanMenu newGame = new HangmanMenu();
+                        break;
+                    } else {
+                        throw new InputMismatchException();
+                    }
+
+                } catch (InputMismatchException e) {
+                    System.out.println("❌ Invalid input! Please enter a number (1 or 2).");
+                    scanner.nextLine();
+                }
             }
+
+
         } else {
             Player player1 = new Player();
             Player player2 = new Player();
-            scanner.nextLine();
             System.out.println("Please enter the name of Player1");
             player1.setName(scanner.nextLine());
             System.out.println("Please enter the name of the second Player2");
             player2.setName(scanner.nextLine());
 
-            System.out.println("what game would you like to play? \n1.Snap \n2.Hangman");
-            int selected = scanner.nextInt();
-            if (selected == 1) {
-                newGame = new Snap(player1, player2);
-                newGame.playSnap(player1, player2);
-            } else if (selected == 2) {
-                HangmanMenu newGame = new HangmanMenu();
+            while (true) {
+                try {
+                    System.out.println("what game would you like to play? \n1.Snap \n2.Hangman");
+                    int selected = scanner.nextInt();
+                    scanner.nextLine();
+                    if (selected == 1) {
+                        newGame = new Snap(player1,player2);
+                        newGame.playSnap(player1,player2);
+                        break;
+                    } else if (selected == 2) {
+                        HangmanMenu newGame = new HangmanMenu();
+                        break;
+                    } else {
+                        throw new InputMismatchException();
+                    }
+
+                } catch (InputMismatchException e) {
+                    System.out.println("❌ Invalid input! Please enter a number (1 or 2).");
+                    scanner.nextLine();
+                }
             }
         }
     }
